1abfbd4319d16651333adb8db8e6533de88bb6c8
kadir <kadirtas02@gmail.com>
2020-06-30 20:45:00 +0300

    First commit

--- app/src/main/java/com/huawei/probation/packagedeliveryapp/map/MapsActivity.java
+++ app/src/main/java/com/huawei/probation/packagedeliveryapp/map/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.huawei.probation.packagedeliveryapp.map;
 
+import com.huawei.hms.location.FusedLocationProviderClient;
+import com.huawei.hms.location.LocationRequest;
 import com.huawei.hms.maps.CameraUpdate;
 import com.huawei.hms.maps.CameraUpdateFactory;
 import com.huawei.hms.maps.HuaweiMap;
@@ -19,18 +21,22 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.util.Random;
+
 /**
  * map activity entrance class
  */
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, HuaweiMap.OnMyLocationButtonClickListener {
 
     private static final String TAG = "MapViewDemoActivity";
 
@@ -50,6 +56,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private Circle mCircle;
 
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest mLocationRequest;
     private static final String[] RUNTIME_PERMISSIONS = {Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
@@ -58,7 +66,34 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         LogM.d(TAG, "map onCreate:");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
+
+
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            Log.i(TAG, "sdk < 28 Q");
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                String[] strings =
+                        {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+                ActivityCompat.requestPermissions(this, strings, 1);
+            }
+        } else {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this,
+                    "android.permission.ACCESS_BACKGROUND_LOCATION") != PackageManager.PERMISSION_GRANTED) {
+                String[] strings = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        "android.permission.ACCESS_BACKGROUND_LOCATION"};
+                ActivityCompat.requestPermissions(this, strings, 2);
+            }
+        }
+
+
 
         if (!hasPermissions(this, RUNTIME_PERMISSIONS)) {
             ActivityCompat.requestPermissions(this, RUNTIME_PERMISSIONS, REQUEST_CODE);
@@ -101,6 +136,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // after call getMapAsync method ,we can get HuaweiMap instance in this call back method
         hmap = map;
         hmap.setMyLocationEnabled(true);
+        hmap.setOnMyLocationButtonClickListener(this);
 
         // move camera by CameraPosition param ,latlag and zoom params can set here
         CameraPosition build = new CameraPosition.Builder().target(new LatLng(60, 60)).zoom(5).build();
@@ -148,5 +184,51 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
         return true;
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    protected static LatLng getLocationInLatLngRad(double radiusInMeters, LatLng currentLocation) {
+        double x0 = currentLocation.longitude;
+        double y0 = currentLocation.latitude;
+
+        Random random = new Random();
+
+        // Convert radius from meters to degrees.
+        double radiusInDegrees = radiusInMeters / 111320f;
+
+        // Get a random distance and a random angle.
+        double u = random.nextDouble();
+        double v = random.nextDouble();
+        double w = radiusInDegrees * Math.sqrt(u);
+        double t = 2 * Math.PI * v;
+        // Get the x and y delta values.
+        double x = w * Math.cos(t);
+        double y = w * Math.sin(t);
+
+        // Compensate the x value.
+        double new_x = x / Math.cos(Math.toRadians(y0));
+
+        double foundLatitude;
+        double foundLongitude;
+
+        foundLatitude = y0 + y;
+        foundLongitude = x0 + new_x;
+
+        return new LatLng(foundLatitude, foundLongitude);
+    }
+
+    public void addMarker() {
+        if (null != mMarker) {
+            mMarker.remove();
+        }
+        MarkerOptions options = new MarkerOptions()
+                .position(new LatLng(48.893478, 2.334595))
+                .title("Hello Huawei Map")
+                .snippet("This is a snippet!");
+        mMarker = hmap.addMarker(options);
     }
 }
